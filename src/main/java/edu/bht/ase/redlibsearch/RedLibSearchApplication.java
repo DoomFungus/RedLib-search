8e1206e3912bf3097bb541bac928a757f61cc784
DoomFungus <unspeakableferretofdoom@gmail.com>
2022-12-08 00:09:41 +0100

    Initial commit

---
+++ src/main/java/edu/bht/ase/redlibsearch/RedLibSearchApplication.java
@@ -0,0 +1,13 @@
+package edu.bht.ase.redlibsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedLibSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedLibSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bht/ase/redlibsearch/RedLibSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bht.ase.redlibsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedLibSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
