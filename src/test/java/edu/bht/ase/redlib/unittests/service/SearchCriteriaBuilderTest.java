70c831f2e34591a3fcbb732775d09d89f09598ab
DoomFungus <unspeakableferretofdoom@gmail.com>
2023-02-19 00:51:24 +0100

    test(search): add search criteria builder exception tests

--- src/test/java/edu/bht/ase/redlib/unittests/service/SearchCriteriaBuilderTest.java
+++ src/test/java/edu/bht/ase/redlib/unittests/service/SearchCriteriaBuilderTest.java
@@ -1,6 +1,8 @@
 package edu.bht.ase.redlib.unittests.service;
 
 import com.mongodb.BasicDBList;
+import edu.bht.ase.redlib.exception.codes.SearchExceptionCodes;
+import edu.bht.ase.redlib.exception.ex.IllegalArgumentException;
 import edu.bht.ase.redlib.service.impl.SearchCriteriaBuilder;
 import org.assertj.core.api.Assertions;
 import org.bson.Document;
@@ -11,14 +13,14 @@ import java.util.List;
 import static edu.bht.ase.redlib.testdata.TestData.*;
 import static edu.bht.ase.redlib.testdata.dto.SearchDtoTestData.*;
 
-public class SearchCriteriaBuilderTest {
+class SearchCriteriaBuilderTest {
     @Test
     void should_ConstructCorrectCriteria_When_GivenEqualsNode() {
         var searchDto = aSearchDto(anEqualsNode());
 
         var criteria = SearchCriteriaBuilder.constructSearchCriteria(searchDto).getCriteriaObject();
 
-        Assertions.assertThat(criteria.get(TEST_SEARCH_EQUALS_DB_FIELD_NAME)).isEqualTo(TEST_SEARCH_EQUALS_VALUE[0]);
+        Assertions.assertThat(criteria).containsEntry(TEST_SEARCH_EQUALS_DB_FIELD_NAME, TEST_SEARCH_EQUALS_VALUE[0]);
     }
 
     @Test
@@ -27,9 +29,9 @@ public class SearchCriteriaBuilderTest {
 
         var criteria = SearchCriteriaBuilder.constructSearchCriteria(searchDto).getCriteriaObject();
 
-        Assertions.assertThat(criteria.containsKey(TEST_SEARCH_CONTAINS_DB_FIELD_NAME)).isTrue();
+        Assertions.assertThat(criteria).containsKey(TEST_SEARCH_CONTAINS_DB_FIELD_NAME);
         Document subCriteria = (Document) criteria.get(TEST_SEARCH_CONTAINS_DB_FIELD_NAME);
-        Assertions.assertThat(subCriteria.get("$all")).isEqualTo(List.of(TEST_SEARCH_CONTAINS_VALUE));
+        Assertions.assertThat(subCriteria).containsEntry("$all", List.of(TEST_SEARCH_CONTAINS_VALUE));
     }
 
     @Test
@@ -38,9 +40,9 @@ public class SearchCriteriaBuilderTest {
 
         var criteria = SearchCriteriaBuilder.constructSearchCriteria(searchDto).getCriteriaObject();
 
-        Assertions.assertThat(criteria.containsKey("$and")).isTrue();
+        Assertions.assertThat(criteria).containsKey("$and");
         BasicDBList subCriteria = (BasicDBList) criteria.get("$and");
-        Assertions.assertThat(subCriteria.size()).isEqualTo(2);
+        Assertions.assertThat(subCriteria).hasSize(2);
     }
 
     @Test
@@ -49,9 +51,9 @@ public class SearchCriteriaBuilderTest {
 
         var criteria = SearchCriteriaBuilder.constructSearchCriteria(searchDto).getCriteriaObject();
 
-        Assertions.assertThat(criteria.containsKey("$or")).isTrue();
+        Assertions.assertThat(criteria).containsKey("$or");
         BasicDBList subCriteria = (BasicDBList) criteria.get("$or");
-        Assertions.assertThat(subCriteria.size()).isEqualTo(2);
+        Assertions.assertThat(subCriteria).hasSize(2);
     }
 
     @Test
@@ -60,8 +62,36 @@ public class SearchCriteriaBuilderTest {
 
         var criteria = SearchCriteriaBuilder.constructSearchCriteria(searchDto).getCriteriaObject();
 
-        Assertions.assertThat(criteria.containsKey("$nor")).isTrue();
+        Assertions.assertThat(criteria).containsKey("$nor");
         BasicDBList subCriteria = (BasicDBList) criteria.get("$nor");
-        Assertions.assertThat(subCriteria.size()).isEqualTo(2);
+        Assertions.assertThat(subCriteria).hasSize(2);
+    }
+
+    @Test
+    void should_ThrowIllegalArgumentException_When_GivenEqualsNodeWithMultipleField() {
+        var equalsNode = anEqualsNode();
+        equalsNode.setField(TEST_SEARCH_CONTAINS_FIELD_NAME);
+        var searchDto = aSearchDto(equalsNode);
+
+        String expectedReasonCode = SearchExceptionCodes.SEARCH_CRITERIA_EQUALS_ILLEGAL.reasonCode;
+        String expectedReasonDescription = SearchExceptionCodes.SEARCH_CRITERIA_EQUALS_ILLEGAL.reasonDescription;
+        Assertions.assertThatThrownBy(() -> SearchCriteriaBuilder.constructSearchCriteria(searchDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .matches(x -> expectedReasonCode.equals(((IllegalArgumentException) x).getReasonCode()))
+                .matches(x -> expectedReasonDescription.equals(((IllegalArgumentException) x).getReasonDescription()));
+    }
+
+    @Test
+    void should_ThrowIllegalArgumentException_When_GivenContainsNodeWithSingleField() {
+        var containsNode = aContainsNode();
+        containsNode.setField(TEST_SEARCH_EQUALS_FIELD_NAME);
+        var searchDto = aSearchDto(containsNode);
+
+        String expectedReasonCode = SearchExceptionCodes.SEARCH_CRITERIA_CONTAINS_ILLEGAL.reasonCode;
+        String expectedReasonDescription = SearchExceptionCodes.SEARCH_CRITERIA_CONTAINS_ILLEGAL.reasonDescription;
+        Assertions.assertThatThrownBy(() -> SearchCriteriaBuilder.constructSearchCriteria(searchDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .matches(x -> expectedReasonCode.equals(((IllegalArgumentException) x).getReasonCode()))
+                .matches(x -> expectedReasonDescription.equals(((IllegalArgumentException) x).getReasonDescription()));
     }
 }
